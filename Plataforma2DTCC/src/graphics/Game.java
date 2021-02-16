6aba32eceb7591cfd5027158067c4d8dc2b799d7
Jhon9191 <joaoantonio15161516@gmail.com>
2021-02-16 11:57:20 -0300

    Creating the game loop

--- Plataforma2DTCC/src/graphics/Game.java
+++ Plataforma2DTCC/src/graphics/Game.java
@@ -13,9 +13,10 @@ public class Game extends Canvas implements Runnable{
 	private static int WIDTH = 160;
 	private static int HEIGHT = 120;
 	private static int SCALE = 3;
+	private Thread thread;
+	private boolean isRunning = true;
 	
-	public Game() {
-		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+	public void initFrame() {
 		jframe = new JFrame("TCC");
 		jframe.add(this);
 		jframe.setResizable(false);
@@ -23,24 +24,62 @@ public class Game extends Canvas implements Runnable{
 		jframe.setLocationRelativeTo(null);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setVisible(true);
+		
+	}
+	
+	public Game() {
+		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		initFrame();
 	}
 	
 	public static void main(String[] args) {
 		Game game = new Game();
+		game.start();
 	}
 
-	public synchronized void tick() {
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+	
+	public synchronized void stop() {
 		
 	}
 	
-	public synchronized void render() {
+	public void tick() {
+		
+	}
+	
+	public void render() {
 		
 	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		long LastTime = System.nanoTime();
+		double amountOfTicks = 60.0f;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		requestFocus();
+		while(isRunning) {	
+			long now = System.nanoTime();
+			delta += ( now - LastTime ) / ns;
+			LastTime = now;
+			if(delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			    }
+				if(System.currentTimeMillis() - timer >= 1000){
+					System.out.println("FPS: "+ frames);
+					frames = 0;
+					timer+=1000;
+				}
+			}
 	}
 
 }
